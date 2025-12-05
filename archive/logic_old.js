6a6cbccc6a8845bec8af54537f0bee3b31149964
Kiza45 <kieransharp2@gmail.com>
2025-12-05 16:57:44 +0000

    new front end layout created and individual lists spawn for each shop that an item is added for

--- archive/logic_old.js
+++ archive/logic_old.js
@@ -63,38 +63,4 @@ itemInput.addEventListener("keypress", function(event) {
 },
 
 
-
-function newList() {
-
-
-  if (inputValue != '') {
-      
-    var storeList = document.createElement("ul");
-    var storeName = document.createElement("header")
-    var inputHeader = document.getElementById("storeInput").value;
-    var t = document.createTextNode(inputHeader);
-    header.appendChild(t);
-    //if (inputValue === '') {
-    //  alert("You must write something!");
-    //} else {
-    // document.getElementById("myUL").appendChild(li);
-    //}
-    document.getElementById("storeInput").value = "";
-
-   // var span = document.createElement("SPAN");
-   // var txt = document.createTextNode("\u00D7");
-   // span.className = "close";
-   // span.appendChild(txt);
-   // li.appendChild(span);
-
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
-}
-} ,
-
-
 );
